0c1ac50ec4ffc710fff0ae6d7bace553b8c20b46
Agus Stiawan <agus.stiawan2211@gmail.com>
2021-06-23 23:23:47 +0700

    Save redux state to local storage

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,26 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './features/user/user.slice';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function getPersistedState(): any {
+  try {
+    const localStorageState = localStorage.getItem('state');
+    if (!localStorageState) return undefined;
+    return JSON.parse(localStorageState);
+  } catch (err) {
+    localStorage.removeItem('state');
+  }
+
+  return undefined;
+}
+
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  reducer: { user: userReducer },
+  preloadedState: getPersistedState(),
+});
+
+store.subscribe(() => {
+  localStorage.setItem('state', JSON.stringify(store.getState()));
 });
 
 export type RootState = ReturnType<typeof store.getState>;
