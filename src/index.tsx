2976aa840c2364d9c8c29094e8f69eea7709e5d2
Agus Stiawan <agus.stiawan2211@gmail.com>
2021-06-24 12:28:58 +0700

    Set up material UI

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import './index.css';
+import '@fontsource/roboto';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
