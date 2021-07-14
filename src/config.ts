1428c3aa3c1c765e205e5c430b35732a7fbb5ca5
Agus Stiawan <agus.stiawan2211@gmail.com>
2021-07-14 18:30:59 +0700

    Change config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 const config = {
-  // GRAPHQL_URI: 'https://laundryku-backend.herokuapp.com/graphql',
-  GRAPHQL_URI: 'http://192.168.43.116:1337/graphql',
+  GRAPHQL_URI: 'https://laundryku-backend.herokuapp.com/graphql',
+  // GRAPHQL_URI: 'http://192.168.43.116:1337/graphql',
 };
 
 export default config;
